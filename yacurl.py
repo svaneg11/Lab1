d7eb1bd0991974d842cace21484d4c3f8edc7d1a
Santiago Vanegas <svaneg11@gmail.com>
2022-02-07 03:09:20 -0500

    add explanation for request, separate headers and body from,beautify http response body, started with download feature

--- yacurl.py
+++ yacurl.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python3
 """Lab 1 TET - por Santiago Vanegas"""
 
 import argparse
+import base64
 import re
 import socket
 import sys
+
+from bs4 import BeautifulSoup
 
 
 class Request:
@@ -34,13 +38,13 @@ class Request:
 
         self.request = f'{self.method} {self.subdirectory} HTTP/1.1\n' \
             f'Host: {self.domain}\n' \
-            f'Origin: ""\n' \
             f'User-Agent: yacurl(svaneg11)\n' \
             f'Accept: */*\n' \
             f'Accept-Language: en-US\n' \
             f'Connection: keep-alive\n\n'
 
         self.request = bytes(self.request, encoding="ascii")
+        explain_request(self.method, self.subdirectory, self.domain)
 
     def __repr__(self):
         return f"HttpRequest({self.method}, {self.domain}, {self.subdirectory}). ServerIP: {self.host} ,\n"\
@@ -50,10 +54,56 @@ class Request:
         return self.host, self.request
 
 
-def send_request(url, port):
+def explain_request(method, subdirectory, domain):
+    verbose_req = f'{method} {subdirectory} HTTP/1.1 --> Request Line - Método: {method}, Ubicación del recurso: {subdirectory}, Version del protocolo: HTTP/1.1\n' \
+    f'Host: {domain} --> Especifica el nombre de dominio del servidor al que se hace la petición.\n' \
+    f'User-Agent: yacurl(svaneg11) --> Especifica la aplicacion que hace la peticion, puede ademas contener version y sistema operativo.\n' \
+    f'Accept: */* --> Tipo de archivo MIME que se espera como respuesta, */* indica cualquier tipo (paginas html, imagenes, videos audio, etc).\n' \
+    f'Accept-Language: en-US --> Indica la preferencia de idioma y localidad para la respuesta.\n' \
+    f'Connection: keep-alive --> Pide al servidor mantener la conexión para seguir enviando datos. Se usa un timeout para detectar cuando ha terminado la transferencia.' \
+    f'\n --> Linea vacia.' \
+    f'\n --> Linea vacia.'
+
+    verbose_req = bytes(verbose_req, encoding="utf-8")
+    print('Request:', verbose_req.decode("utf-8"), sep='\n', end='\n\n')
+
+
+def separate_response(response):
+    response = response.replace('\r', '')   # remove windows carriage return from response
+    match = re.search('(.+?\n\n)(.*)', response, flags=re.DOTALL)   # retrieve the http headers and the body
+
+    if match:
+        http_headers = match.group(1)
+        body = match.group(2)
+
+        if body and 'Transfer-Encoding: chunked' in http_headers:
+            body = re.sub('\n?(^[0-9a-z]+$)', '', body, flags=re.MULTILINE)  # Remove chunksize numbers from body
+
+        match = re.search('Content-Type:\s*([^;\n]+)', http_headers)   # Get the Content-Type
+        if match:
+            content_type = match.group(1)
+
+            if (content_type == 'text/html'):
+                soup = BeautifulSoup(body, "html.parser")
+                body = soup.prettify()
+    return http_headers, body, content_type
+
+def save_file(body, filename, content_type):
+    print(content_type , len(content_type))
+    if content_type == 'text/html':
+        if '.html' not in filename:
+            filename += '.html'
+        file = open(filename, 'wt')
+        file.write(body)
+        file.close()
+    if content_type == 'image/jpeg':
+        if '.jpg' not in filename and'.jpeg' not in filename:
+            filename += '.jpg'
+
+def send_request(url, port, filename):
     request = Request(url)
-    repr(request)
     HOST, http_request = request.get()
+
     PORT = port      # The port used by the server
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
@@ -64,10 +114,12 @@ def send_request(url, port):
             while True:
                 data = s.recv(1024)
                 response += data
-        except Exception as e:
+        except Exception:
             s.close()
             response = response.decode('ISO-8859-1').encode('utf-8').decode('utf-8')
-            print('Response:', response, sep='\n')
+            http_headers, body, content_type = separate_response(response)
+            print(http_headers, body, sep='\n\n')
+            save_file(body, filename, content_type)
             sys.exit(0)
 
 
@@ -75,5 +127,7 @@ parser = argparse.ArgumentParser(description='HTTP requests using sockets')
 parser.add_argument('url', help='The url where the request is going to be sent.')
 parser.add_argument('--port', type=int, default=80,
                     help='Use the specified port number (default is port 80).')
+parser.add_argument('--filename', default='file',
+                    help='Name to use for the file in which the body(if any) will be saved')
 args = parser.parse_args()
-send_request(args.url, args.port)
+send_request(args.url, args.port, args.filename)
