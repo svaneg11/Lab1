2953a95bbb8de79cf6050c2d495670b99c9a0ba4
Santiago Vanegas <svaneg11@gmail.com>
2022-02-13 00:43:00 -0500

    Add support for binary files, add explanation of response headers, add finding of static resources from html pages

--- yacurl.py
+++ yacurl.py
@@ -43,7 +43,7 @@ class Request:
             f'Connection: keep-alive\r\n\r\n'
 
         self.request = bytes(self.request, encoding="'ISO-8859-1'")
-        #explain_request(self.method, self.subdirectory, self.domain)
+        explain_request(self.method, self.subdirectory, self.domain)
 
     def __repr__(self):
         return f"HttpRequest({self.method}, {self.domain}, {self.subdirectory}). ServerIP: {self.host} ,\n"\
@@ -57,14 +57,44 @@ def explain_request(method, subdirectory, domain):
     verbose_req = f'{method} {subdirectory} HTTP/1.1 --> Request Line - Método: {method}, Ubicación del recurso: {subdirectory}, Version del protocolo: HTTP/1.1\n' \
     f'Host: {domain} --> Especifica el nombre de dominio del servidor al que se hace la petición.\n' \
     f'User-Agent: yacurl(svaneg11) --> Especifica la aplicacion que hace la peticion, puede ademas contener version y sistema operativo.\n' \
-    f'Accept: */* --> Tipo de archivo MIME que se espera como respuesta, */* indica cualquier tipo (paginas html, imagenes, videos audio, etc).\n' \
+    f'Accept: */* --> Tipo de archivo MIME que se espera como respuesta en el cuerpo del mensaje, */* indica cualquier tipo (paginas html, imagenes, videos audio, etc).\n' \
     f'Accept-Language: en-US --> Indica la preferencia de idioma y localidad para la respuesta.\n' \
-    f'Connection: keep-alive --> Pide al servidor mantener la conexión para seguir enviando datos. Se usa un timeout para detectar cuando ha terminado la transferencia.' \
-    f'\n --> Linea vacia.' \
-    f'\n --> Linea vacia.'
+    f'Connection: keep-alive --> Pide al servidor mantener la conexión para seguir enviando datos. Se usa un timeout para detectar cuando ha terminado la transferencia.\n' \
+    f'--> Linea vacia.\n'
 
     verbose_req = bytes(verbose_req, encoding="utf-8")
     print('Request:', verbose_req.decode("utf-8"), sep='\n', end='\n\n')
+
+
+def explain_response(response_headers):
+    lines = response_headers.splitlines()
+    response_line = lines[0]
+    match = re.match('([^ ]+) ([^ ]+) (.*)', response_line)
+    version, status_code, status = match.group(1), match.group(2), match.group(3),
+    headers = dict()
+    for line in lines[1:-1]:    # Parse headers
+        index = line.index(':')
+        key = line[:index]
+        value = line[index+1:]
+        headers[key] = value
+
+    explain_headers = dict()
+    explain_headers['Date'] = 'Fecha en que se originó la respuesta, en GMT.'
+    explain_headers['Server'] = 'Nombre del software que atendió la petición.'
+    explain_headers['Content-Length'] = 'Indica el tamaño del cuerpo de la respuesta en bytes.'
+    explain_headers['Content-Type'] = 'El tipo MIME del archivo que se envía como respuesta en el cuerpo del mensaje y opcionalmente el charset si es texto.'
+
+    print('Response:')
+    print(response_line, f'--> Response Line - Version del protocolo: {version}, Código de estado: {status_code}, Mensaje de estado: {status}')
+    for k, v in headers.items():
+        if k in explain_headers.keys():
+            print(f'{k}:{v} --> {explain_headers[k]}')
+        else:
+            print(f'{k}:{v} -->')
+    print('')
+
+
+
 
 
 def separate_response(response):
@@ -88,35 +118,35 @@ def separate_response(response):
             return http_headers, body, content_type
 
 def save_file(response_bytes, body, filename, content_type):
-    print(content_type , len(content_type))
     if content_type == 'text/html':
         if '.html' not in filename:
             filename += '.html'
-        file = open(filename, 'wt')
+        file = open(filename, 'wt', encoding='utf-8')
         file.write(body)
         file.close()
-    if content_type == 'image/jpeg':
-        if '.jpg' not in filename and'.jpeg' not in filename:
+    elif content_type == 'image/jpeg':
+        if '.jpg' not in filename and '.jpeg' not in filename:
             filename += '.jpg'
-    if content_type == 'image/gif':
-        if '.gif' not in filename:
-            filename += '.gif'
-    if content_type == 'image/jpeg' or content_type == 'image/gif':
+    else:
+        extension = '.' + content_type[content_type.index('/')+1:]
+        if extension not in filename:
+            filename += extension
+
+    if content_type != 'text/html':
         sep = b''
         count = 0
         while sep != b'\r\n\r\n':   # find binary body
             sep = response_bytes[count:count + 4]
-            print(sep, count)
             count += 1
         count += 3
+        print('Body:')
         print(response_bytes[count:])
-        img = open(filename, mode='wb')
+        img = open(filename, mode='wb', encoding='utf-8')
         img.write(response_bytes[count:])
 
 
 def send_request(url, port, filename):
     request = Request(url)
-    #print(repr(request))
     HOST, http_request = request.get()
     PORT = port      # The port used by the server
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -132,11 +162,31 @@ def send_request(url, port, filename):
                 response_bytes += data
         except Exception:
             pass
-        print(response_bytes)
         s.close()
         response = response_bytes.decode('ISO-8859-1').encode('utf-8').decode('utf-8')
-        http_headers, body, content_type = separate_response(response)
-        print(http_headers, body, sep='\n\n')
+        tup = separate_response(response)
+        if tup:
+            http_headers, body, content_type = tup
+        else:
+            http_headers = response
+            body = None
+            content_type = None
+        explain_response(http_headers)
+        if content_type == 'text/html':
+            print('Body:', body, sep='\n')
+            soup = BeautifulSoup(body, "html.parser")
+            images = soup.findAll('img')
+            links = []
+            for image_tag in images:
+                if image_tag['src'] not in links:
+                    links.append(image_tag['src'])
+
+            print('Recursos Estáticos:')
+            for l in links:
+                if l.startswith('/'):
+                    print('http://',request.domain, l, sep='')
+                else:
+                    print(l)
         save_file(response_bytes, body, filename, content_type)
         sys.exit(0)
 
